9fe80c7aca9a66295053cd1f2ab3e8c3efcc1590
manudelmarche <manu.delmarche@gmail.com>
2019-07-01 19:43:47 +0200

    After Lessoon 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MusicEventComponent } from './music-event/music-event.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,11 @@ import { MusicEventComponent } from './music-event/music-event.component';
     MusicEventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
