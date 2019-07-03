250b8def5c9215dfc43db512fb992f92ae2f6b25
manudelmarche <manu.delmarche@gmail.com>
2019-07-03 13:30:11 +0200

    After lesson 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MusicEventComponent } from './music-event/music-event.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule} from '@angular/material';
 import { PriceRangePipe } from './price-range.pipe';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicEventComponent,
-    PriceRangePipe
+    PriceRangePipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    FormsModule
+  ],
+  exports: [
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Cancel() {
+    this.username='';
+    this.password='';
+  }
+
+  Login() {
+    this.username='';
+    this.password='';
+  }
+
+}
